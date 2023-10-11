a904acef9041cef503a56e2e01abb5176066582e
alaa-3aziz <alaa.3aziz4@gmail.com>
2023-10-11 15:08:33 -0700

    commit

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ *main - Entry point
+ *
+ *Return: Always 0 (success)
+*/
+
+int main(void)
+{
+	int count;
+	unsigned long fin1 = 0, fib2 = 1, sum;
+
+	for (count = 0; count < 50; count++)
+	{
+		sum = fib1 + fib2;
+		printf("%lu", sum);
+
+		fin1 = fib2;
+		fib2 = sum;
+
+		if (count == 49)
+			printf("\n");
+		else
+			printf(", ");
+	}
+	return (0);
+}
