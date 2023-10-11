03c451a0ac3da1e5dc676a641fa8ed4c799d10e4
alaa-3aziz <alaa.3aziz4@gmail.com>
2023-10-11 12:20:14 -0700

    commit

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ *print_alphabet_x10 - function that will print the alphabet x 10
+ *
+*/
+
+void print_alphabet_x10(void)
+{
+	int line, ch;
+
+	for (line = 0; line <= 9; ch++)
+		_putchar(ch);
+	_putchar('\n');
+}
